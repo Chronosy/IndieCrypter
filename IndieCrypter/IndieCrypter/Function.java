c202ffa27897113a465b571e179a24c6c37bff59
AllahuAkbar <AllahuAkbar@Takbir>
2015-12-11 07:08:48 +0900

    Fix Major bug exists in indexing

--- IndieCrypter/IndieCrypter/Function.java
+++ IndieCrypter/IndieCrypter/Function.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Function {
 	//JfileChooser will be used when to pop up selecting file or creating new files
@@ -35,15 +36,16 @@ public class Function {
 	
 	//Text Encryption is Simple! set password and return what is encrypted!
 	public String Encryption(String input, String passwd){
-		AES.setKey(passwd);				
-		return AES.Encrypt(input);
+		AES.setKey(passwd);	
+		return StringtoBinary(AES.Encrypt(input));
 	}
 	public String Decryption(String input, String passwd){
 		AES.setKey(passwd);
-		return AES.Decrypt(input);
+		return AES.Decrypt(BinarytoString(input));
 	}
 	
 	// Import from a text file to be chosen
+	
 	public String Import(){
 		StringBuilder sB = new StringBuilder();
 		String temp = "";
@@ -51,8 +53,7 @@ public class Function {
 			try {
 				FileInputStream fIS = new FileInputStream(fC.getSelectedFile());
 				InputStreamReader iSR = new InputStreamReader(fIS, "UTF-8");
-				BufferedReader bR = new BufferedReader(iSR);
-				
+				BufferedReader bR = new BufferedReader(iSR);				
 				try{
 					while((temp=bR.readLine())!=null) {
 						sB.append(temp);
@@ -66,20 +67,6 @@ public class Function {
 			} catch (Exception e) {
 				JOptionPane.showMessageDialog(null, "file not FOUND");
 			}
-			
-				/*try {
-					file = fC.getSelectedFile();
-					Scanner input = new Scanner(file, "UTF-8");		//If not UTF-8, it will not read certain alian characters!
-					while(input.hasNext()){
-						sB.append(input.nextLine());
-						sB.append(System.lineSeparator());
-					}
-					input.close();
-					return sB.toString();
-				}catch (FileNotFoundException e) {
-					JOptionPane.showMessageDialog(null, "File cannot be READ! Try other file");
-					Import();
-				}*/
 		}else{
 			JOptionPane.showMessageDialog(null, "Select Canceled");
 			return "String to be Encrypted or Decrypted";
@@ -93,10 +80,7 @@ public class Function {
 				try{
 					file = new File(fC.getSelectedFile()+".txt");
 					BufferedWriter br= new BufferedWriter(new 
-					OutputStreamWriter(new FileOutputStream(file,true), "UTF-8"));
-					/*FileOutputStream fIS = new FileOutputStream(fC.getSelectedFile());
-					OutputStreamWriter oSR = new OutputStreamWriter(fIS, "UTF-8");
-					BufferedWriter bR = new BufferedWriter(oSR);*/
+					OutputStreamWriter(new FileOutputStream(file,true)));
 					br.write(result);
 					br.flush();
 					br.close();
@@ -124,78 +108,84 @@ public class Function {
 	// If use Jpeg format, when the image is exported, it will encoded with its own algorithm, causing lost of data stored in the image
 	// Thus, This will use png format, which is lossless format.
 	public void StegaEnc(String input, String passwd){
-		
+		int counter= 0;
 		AES.setKey(passwd);
-		String encData = AES.Encrypt(input);	//After encrypt the given data, 
-		encData = StringtoBinary(encData);		//convert it to string of binary.
-		
+		String encData = StringtoBinary(AES.Encrypt(input));	//encrypt the given data,
 		//Code below is used for total height for hiding place.
 		//Note that size of encData increases 2^n
-		String encDataSize = String.format("%32s", Integer.toBinaryString(encData.length()/32)); 
-		
+		String encDataSize = String.format("%32s", Integer.toBinaryString(encData.length()/32));
+		encDataSize=encDataSize.replace(' ', '0');
 		/*  width is set to 32!
 		 *  set first column to save the total height of hiding place
 		 * 	Storing information on pixel is done by Modifying LSB of the pixel
 		 *  (which is LSB of blue value)
 		 */
 		for(int i = 0; i < 32; i++){ 			// first 32bits of first column is saving info of data size
-			String temp = intToBinary(image.getRGB(i, 0));
+			String temp = intToBinary(image.getRGB(i, image.getHeight()-1));
 			if(encDataSize.charAt(i)=='1'){
 				temp = temp.substring(0,31)+'1';
 			}else{
 				temp = temp.substring(0,31)+'0';
 			}
-			image.setRGB(i, 0, binaryToInt(temp));	
+			image.setRGB(i, image.getHeight()-1, binaryToInt(temp));	
 		}
 		/*There could be two cases, whether inputsize is greater than the height of the image, or not
 		 * In the first case, once it reaches to the height, it will continue to save to data in next row
 		 * If datasize is greater than h * w of the image, it will throw exception.
 		 */
 		try{
-			int numOfCol = encData.length()/32;
-			if(image.getHeight() < numOfCol){
-				int numOfRow = (numOfCol/image.getHeight());
-				int iTemp = 0;
-				while(iTemp < numOfRow+1){
-					if(iTemp < numOfRow){
-						for(int i = 1; i < image.getHeight(); i++){
-							for(int j = iTemp*32; j<32+(32*iTemp); j++){
-								String temp = intToBinary(image.getRGB(j, i));
-								temp = temp.substring(0,31)+ encData.charAt(j+((i-1)*32));
-								image.setRGB(j, i, binaryToInt(temp));
-							}
-						}
-					}else{
-						for(int i = 1; i <(numOfCol-(numOfRow*image.getHeight())+1); i++){
-							for(int j = iTemp*32; j<32+(32*iTemp); j++){
-								String temp = intToBinary(image.getRGB(j, i));
-								temp = temp.substring(0,31)+ encData.charAt(j+((i-1)*32));
-								image.setRGB(j, i, binaryToInt(temp));
-							}
-						}
-					}
-					iTemp++;
-				}
-			}else{
-				for(int i = 1; i <numOfCol+1; i++){
-					for(int j = 0; j<32; j++){
-						String temp = intToBinary(image.getRGB(j, i));
-						temp = temp.substring(0,31)+ encData.charAt(((i-1)*32)+j);
+			String temp = "";
+			int colSize = encData.length()/32;	
+			if(colSize < image.getHeight()){		//case input is small enough to hide in a row.
+				for(int i = 0; i < colSize; i++){
+					for(int j = 0; j < 32; j++){
+						temp = intToBinary(image.getRGB(j, i));
+						temp = temp.substring(0, 31) + encData.charAt(j+(i*32));
 						image.setRGB(j, i, binaryToInt(temp));
 					}
 				}
+			}else{
+				int rowSize = colSize/image.getHeight();
+				for(int countRow = 0; countRow < rowSize+1; countRow++){
+					if(countRow == rowSize){
+						for(int i = 0; i < (colSize-(image.getHeight()*rowSize)+rowSize); i++){
+							for(int j = countRow*32; j < 32+(countRow*32); j++){
+								temp = intToBinary(image.getRGB(j, i));
+								temp = temp.substring(0, 31) + encData.charAt(counter);
+								image.setRGB(j, i, binaryToInt(temp));
+								counter++;
+							}
+						}
+					}else{
+						for(int i = 0; i < image.getHeight()-1; i++){
+							for(int j = countRow*32; j < 32+(countRow*32); j++){
+								temp = intToBinary(image.getRGB(j, i));
+								temp = temp.substring(0, 31) + encData.charAt(counter);
+								image.setRGB(j, i, binaryToInt(temp));
+								counter++;
+							}
+						}
+					}
+				}
 			}
+			
+			
 		}catch(Exception e){
 				JOptionPane.showMessageDialog(null, "Resolution of the image is too small! Try with other picture!");
 		}
 	//Exporting image as png.
-		if(fC1.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
-			try{
-				File file = new File(fC1.getSelectedFile(),"");
-				ImageIO.write(image, "png", file);
-			}catch(Exception e){
-				JOptionPane.showMessageDialog(null, "ERRRROORRRR");
+		if(encData.length() ==counter){
+			if(fC1.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+				try{
+					File file = new File(fC1.getSelectedFile(),"");
+					ImageIO.write(image, "png", file);
+				}catch(Exception e){
+					JOptionPane.showMessageDialog(null, "ERRRROORRRR");
+				}
 			}
+		}else{
+			System.out.println(counter);
+			JOptionPane.showMessageDialog(null, "PLZ don exploit the program..");
 		}
 	}
 	
@@ -204,40 +194,37 @@ public class Function {
 		//The name of the file will be the password.
 		AES.setKey(file.getName().substring(0,file.getName().length()-4));
 		String length = "",data="";
-		
 		//reading total height
 		for(int i = 0; i<32; i++){
-			length = length + intToBinary(image.getRGB(i, 0)).charAt(31);
-		}
+			length = length + intToBinary(image.getRGB(i, image.getHeight()-1)).charAt(31);
+		}int colSize = binaryToInt(length);
 		//retriving the data from the pixels
 		// same algorithm from StagaEnc(), except exception statement is not needed
-		int dataSize = binaryToInt(length);
-		if(dataSize > image.getHeight()){
-			int numOfRow = dataSize/image.getHeight();
-			int iTemp = 0;
-			while(iTemp < numOfRow+1){
-				if(iTemp < numOfRow){
-					for(int i = 1; i<image.getHeight(); i++){
-						for(int j =iTemp*32; j < 32+(iTemp*32); j++){
+		if(colSize < image.getHeight()){
+			for(int i = 0; i < colSize; i++){
+				for(int j = 0; j < 32; j++){
+					String temp = intToBinary(image.getRGB(j,i));
+					data = data + temp.charAt(31);
+				}
+			}
+		}else{
+			int rowSize = colSize/image.getHeight();
+
+			for(int countRow = 0; countRow < rowSize+1; countRow++){
+				if(countRow == rowSize){
+					for(int i = 0; i < (colSize-(image.getHeight()*rowSize)+rowSize); i++){
+						for(int j = countRow*32; j < 32+(countRow*32); j++){
 							String temp = intToBinary(image.getRGB(j,i));
 							data = data + temp.charAt(31);
 						}
 					}
 				}else{
-					for(int i = 1; i<(dataSize-(numOfRow*image.getHeight())+1); i++){
-						for(int j = iTemp*32; j < 32+(iTemp*32); j++){
+					for(int i = 0; i< image.getHeight()-1; i++){
+						for(int j = countRow*32; j < 32+(countRow*32); j++){
 							String temp = intToBinary(image.getRGB(j,i));
 							data = data + temp.charAt(31);
 						}
 					}
-				}
-				iTemp++;
-			}
-		}else{
-			for(int i = 1; i<dataSize; i++){
-				for(int j = 0; j < 32; j++){
-					String temp = intToBinary(image.getRGB(j,i));
-					data = data + temp.charAt(31);
 				}
 			}
 		}
